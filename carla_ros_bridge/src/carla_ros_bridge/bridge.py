5653db9e7e2332f40d36c0c56e3153d184439537
makaveli10 <suryanvineet47@gmail.com>
2022-04-06 08:00:54 +0530

    refactor; try -> if for lidar fetch

--- carla_ros_bridge/src/carla_ros_bridge/bridge.py
+++ carla_ros_bridge/src/carla_ros_bridge/bridge.py
@@ -310,9 +310,9 @@ class CarlaRosBridge(CompatibleNode):
         for parent, rgb_cams in self.rgb_cams.items():
             # check if lidar exists for the same parent
             curr_lidar = None
-            try:
+            if parent in self.lidar:
                 curr_lidar = self.lidar[parent]
-            except Exception as e:
+            else:
                 self.node.logwarn(f"lidar is None for {parent}")
 
             for id_, rgb_cam in rgb_cams.items():
