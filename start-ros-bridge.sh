64e7a881f55489855b7632c48d10c8d01bea2230
Jakub Piotr Cłapa <jpc@collabora.com>
2022-05-03 01:56:00 +0200

    Added support for different carla hostnames (required for JupyterHub)

--- start-ros-bridge.sh
+++ start-ros-bridge.sh
@@ -4,6 +4,6 @@ set -e
 # setup ros environment
 source "/opt/carla-ros-bridge/devel/setup.bash"
 
-python3 -u /wait-for-carla.py carla
+python3 -u /wait-for-carla.py "${CARLA_HOSTNAME:-carla}"
 
-exec roslaunch carla_ros_bridge carla_ros_bridge.launch
+exec roslaunch carla_ros_bridge carla_ros_bridge.launch host:="${CARLA_HOSTNAME:-carla}"
