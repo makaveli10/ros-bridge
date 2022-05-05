03ccd03b7e1c36ed71e09b3a58238cdcdb8898da
Jakub Piotr Cłapa <jpc@collabora.com>
2022-05-05 14:12:46 +0200

    Added ROSBRIDGE_WEBSOCKET_EXTERNAL_PORT to be able to proxy the websocket

--- start-ros-bridge.sh
+++ start-ros-bridge.sh
@@ -6,4 +6,6 @@ source "/opt/carla-ros-bridge/devel/setup.bash"
 
 python3 -u /wait-for-carla.py "${CARLA_HOSTNAME:-carla}"
 
-exec roslaunch carla_ros_bridge carla_ros_bridge.launch host:="${CARLA_HOSTNAME:-carla}"
+exec roslaunch carla_ros_bridge carla_ros_bridge.launch \
+  host:="${CARLA_HOSTNAME:-carla}" \
+  ${ROSBRIDGE_WEBSOCKET_EXTERNAL_PORT:+"websocket_external_port:=${ROSBRIDGE_WEBSOCKET_EXTERNAL_PORT}"}
