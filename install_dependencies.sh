5232d4d02e11df1e34e23679b6a4e65bdd1dc390
makaveli10 <suryanvineet47@gmail.com>
2023-03-15 18:17:34 +0800

    update requirements

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -34,5 +34,9 @@ sudo apt-get install --no-install-recommends -y \
     python-is-python3 \
     $ADDITIONAL_PACKAGES
 
-python3 -m pip install --upgrade pip
-python$PYTHON_SUFFIX -m pip install -r $SCRIPT_DIR/requirements.txt
+python -m pip install --upgrade pip
+python -m pip install -r $SCRIPT_DIR/requirements.txt
+python3 -m pip install torch==1.10.1+$CUDA_VER torchvision==0.11.2+$CUDA_VER torchaudio==0.10.1 -f https://download.pytorch.org/whl/$CUDA_VER/torch_stable.html
+python -m pip install mmcv-full==1.4.0 -f https://download.openmmlab.com/mmcv/dist/$CUDA_VER/torch1.10.1/index.html
+python -m pip install mmdet==2.20
+python -m pip install --upgrade pyOpenSSL
